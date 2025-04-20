b5d6346e2de3a73e0829cc7f3b49752b91b90799
OsipenkoSA <os_s@bk.ru>
2025-04-20 17:11:51 +0300

    20_04_25

---
+++ DZ_20_04_25_Osipenko_S_A/DZ_20_04_25_Osipenko_S_A.py
@@ -0,0 +1,9 @@
+import re
+
+
+def validate_password(password):
+    return re.findall(r"^[A-Za-z0-9@_-]{6,18}$", password)
+
+
+print(validate_password("my-p@ssw0rd_"))
+print(validate_password("my-p@ssw0rd+"))
