20d3e301434c662021233d554923b206b38a0f86
OsipenkoSA <os_s@bk.ru>
2025-06-14 16:32:09 +0300

    14.06.25 Lesson

--- main.py
+++ main.py
@@ -6025,10 +6025,10 @@
 # print(row)
 
 # ================================================================
-import requests
-from bs4 import BeautifulSoup
-import re
-import csv
+# import requests
+# from bs4 import BeautifulSoup
+# import re
+# import csv
 
 
 # def get_html(url):
@@ -6145,5 +6145,25 @@ import csv
 #
 # if __name__ == "__main__":
 #     main()
+
+# ===============================================================================
+# 14.06.25
+
+import socket
+
+
+def run():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', 5000))
+    server_socket.listen()
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        request = client_socket.recv(1024)
+        print(f"Клиент {addr} => \n{request}\n")
+
+
+if __name__ == '__main__':
+    run()
 
 
