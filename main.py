9c32838a8033eb03a97867b9df24ee1dd1a2448c
OsipenkoSA <os_s@bk.ru>
2025-06-19 15:25:09 +0300

    19.06.25 DZ

--- main.py
+++ main.py
@@ -6147,23 +6147,89 @@
 #     main()
 
 # ===============================================================================
-# 14.06.25
+# 14.06.25 - 15.06.25  серверный сокет и методы
 
-import socket
+# import socket
+# from view import index, blog
+#
+#
+# URLS = {
+#     '/': index,
+#     '/blog': blog
+# }
+#
+#
+# def parse_request(request):
+#     parsed = request.split()
+#     method = parsed[0]
+#     url = parsed[1]
+#     return method, url
+#
+#
+# def generate_headers(method, url):
+#     if method != "GET":
+#         return 'HTTP/1.1 405 Method Not Allowed!\n\n', 405
+#     if url not in URLS:
+#         return 'HTTP/1.1 404 Page Not Found!\n\n', 404
+#     return 'HTTP/1.1 200 OK\n\n', 200
+#
+#
+# def generate_content(code, url):
+#     if code == 404:
+#         return "<h1>404</h1><h3>Page Not Found</h3>"
+#     elif code == 405:
+#         return "<h1>405</h1><h3>Method Not Allowed</h3>"
+#     return URLS[url]()
+#
+#
+# def generate_response(request):
+#     method, url = parse_request(request)
+#     headers, code = generate_headers(method, url)
+#     body = generate_content(code, url)
+#     return (headers + body).encode()
+#
+#
+# def run():
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind(('127.0.0.1', 5000))  # привязывает сокет к адресу
+#     server_socket.listen()  # переводит сервер в режим приема соединений
+#
+#     while True:
+#         client_socket, addr = server_socket.accept()  # принимает соединение и блокирует приложение в ожидание сообщения от клиента
+#         request = client_socket.recv(1024)  # читает и возвращает данные в двоичном формате из сокета в (макс число байтов в сообщении)
+#         print(f"Клиент {addr} => \n{request.decode('utf-8')}\n")
+#
+#         response = generate_response(request.decode())
+#         client_socket.sendall(response)  # отправляет все данные клиенту
+#         client_socket.close()  # закрывает сокет
+#
+#
+# if __name__ == '__main__':
+#     run()
 
+# =========================================================================================
+# базы данных
 
-def run():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('127.0.0.1', 5000))
-    server_socket.listen()
+import sqlite3
 
-    while True:
-        client_socket, addr = server_socket.accept()
-        request = client_socket.recv(1024)
-        print(f"Клиент {addr} => \n{request}\n")
+# con = sqlite3.connect("profile.db")
+# cur = con.cursor()
+#
+# cur.execute("""
+# """)
+#
+# con.close()
+# ================ то же самое
 
+with sqlite3.connect("profile.db") as con:
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    summa REAL,
+    date TEXT
+    )""")
 
-if __name__ == '__main__':
-    run()
+    cur.execute("DROP TABLE users")
 
 
