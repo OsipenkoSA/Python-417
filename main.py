6e70dea28397614e4793ca8ef44a66d2b8fc9c42
OsipenkoSA <os_s@bk.ru>
2025-06-19 15:27:18 +0300

    19.06.25 comment

--- main.py
+++ main.py
@@ -6210,7 +6210,7 @@
 # =========================================================================================
 # базы данных
 
-import sqlite3
+# import sqlite3
 
 # con = sqlite3.connect("profile.db")
 # cur = con.cursor()
@@ -6221,15 +6221,15 @@ import sqlite3
 # con.close()
 # ================ то же самое
 
-with sqlite3.connect("profile.db") as con:
-    cur = con.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    summa REAL,
-    date TEXT
-    )""")
-
-    cur.execute("DROP TABLE users")
+# with sqlite3.connect("profile.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""CREATE TABLE IF NOT EXISTS users(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     summa REAL,
+#     date TEXT
+#     )""")
+#
+#     cur.execute("DROP TABLE users")
 
 
