28ce9c96ed2ea5b263288efb124b0843703ec3d8
OsipenkoSA <os_s@bk.ru>
2025-07-29 11:39:18 +0300

    28.07.25

--- first/password_generator_project/generator/views.py
+++ first/password_generator_project/generator/views.py
@@ -8,21 +8,24 @@ def home(request):
     return render(request, "generator/home.html", {'lst': lst})
 
 
+def description(request):
+    return render(request, "generator/description.html")
+
+
 def password(request):
     char = [chr(i) for i in range(97, 123)]
     length = int(request.GET.get('length'))
-    uppercase = request.GET.get('uppercase')
-    numbers = request.GET.get('numbers')
-    special = request.GET.get('special')
-    if uppercase:
-        char += [chr(i) for i in range(65, 91)]
-    if numbers:
-        char += [chr(i) for i in range(48, 58)]
-    if special:
-        char += [chr(i) for i in range(33, 48)]
-        char += [chr(i) for i in range(58, 65)]
-        char += [chr(i) for i in range(91, 97)]
-        char += [chr(i) for i in range(123, 127)]
+    if request.GET.get('uppercase'):
+        char.extend([chr(i) for i in range(65, 91)])
+
+    if request.GET.get('numbers'):
+        char.extend([chr(i) for i in range(48, 58)])
+
+    if request.GET.get('special'):
+        char.extend([chr(i) for i in range(33, 48)])
+        char.extend([chr(i) for i in range(58, 65)])
+        char.extend([chr(i) for i in range(91, 97)])
+        char.extend([chr(i) for i in range(123, 127)])
 
     psw = ''
     for i in range(length):
