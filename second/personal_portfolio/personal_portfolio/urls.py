3c3c0eaef410712fec90ca6507d3bbff99d65598
OsipenkoSA <os_s@bk.ru>
2025-08-17 15:44:08 +0300

    17.08.25 lesson

--- second/personal_portfolio/personal_portfolio/urls.py
+++ second/personal_portfolio/personal_portfolio/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.global_settings import MEDIA_ROOT
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from skills import views
@@ -24,6 +24,7 @@ from skills import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
