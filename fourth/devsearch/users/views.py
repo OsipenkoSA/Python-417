2a294065d7867900d1c636d97601d06b90fe8c5c
OsipenkoSA <os_s@bk.ru>
2025-08-31 13:11:16 +0300

    31.08.25

---
+++ fourth/devsearch/users/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Profile
+
+
+def profiles(request):
+    prof = Profile.objects.all()
+    context = {
+        'profiles': prof
+    }
+    return render(request, 'users/index.html', context)
+
+
+def user_profile(request, pk):
+    prof = Profile.objects.get(id=pk)
+
+    top_skills = prof.skill_set.exclude(description__exact="")  # __exact -точное совпадение
+    other_skill = prof.skill_set.filter(description="")
+    context = {
+        'profile': prof,
+        'top_skills': top_skills,
+        'other_skill': other_skill
+    }
+    return render(request, 'users/profile.html', context)
