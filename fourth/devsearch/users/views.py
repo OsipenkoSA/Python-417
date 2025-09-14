d6ad21d1a27ebd79c85fb81f848527727973936d
OsipenkoSA <os_s@bk.ru>
2025-09-14 17:14:20 +0300

    lesson 13.09.25

--- fourth/devsearch/users/views.py
+++ fourth/devsearch/users/views.py
@@ -7,14 +7,16 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 from django.contrib.auth.decorators import login_required
-from .utils import search_profiles
+from .utils import search_profiles, paginate_profiles
 
 
 def profiles(request):
     prof, search_query = search_profiles(request)
+    custom_range, prof = paginate_profiles(request, prof, 3)
     context = {
         'profiles': prof,
-        'search_query': search_query
+        'search_query': search_query,
+        'custom_range': custom_range
     }
     return render(request, 'users/index.html', context)
 
