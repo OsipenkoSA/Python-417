4bb4125e23f883fafab0816427f5fdaef3d13ddf
OsipenkoSA <os_s@bk.ru>
2025-09-21 19:07:23 +0300

    19.07.25

--- fifth/programming_blog/blog/urls.py
+++ fifth/programming_blog/blog/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', BlogHome.as_view(), name='index'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
 ]
--- fifth/programming_blog/programming_blog/settings.py
+++ fifth/programming_blog/programming_blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -103,19 +104,23 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
